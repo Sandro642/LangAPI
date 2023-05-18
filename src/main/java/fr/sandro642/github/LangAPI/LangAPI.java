39d0b139e166a2b315952d52d759116ff9727bc3
Sandro642 <sandro33810@gmail.com>
2023-05-18 22:45:32 +0200

    Setup API Lang

--- src/main/java/fr/sandro642/github/LangAPI/LangAPI.java
+++ src/main/java/fr/sandro642/github/LangAPI/LangAPI.java
@@ -1,40 +1,41 @@
 package fr.sandro642.github.LangAPI;
 
-import fr.sandro642.github.Messages.Messages;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.List;
 
 public class LangAPI extends JavaPlugin {
 
-    private static LangAPI instance;
-    public static String lang = "EN"; // Language
+    private static LangAPI setuplang;
 
     public static LangAPI getInstance() {
-        return instance;
+        return setuplang;
     }
 
     ///
 
-    public void setupMessages(Boolean setup) {
+    public static LangAPI PrimaryLanguages(String lang) {
         /**
-         * Set up the LangAPI
+         * Set up the primary lang
+         *
+         * FR
+         * EN
          */
 
-        if (setup.equals(true)) {
+        // TODO: Add the primary lang
 
+        return null;
+    }
 
-            Messages.load();
+    public static LangAPI SecondaryLanguages(String lang) {
+        /**
+         * Set up the secondary lang
+         *
+         * FR
+         * EN
+         */
 
-            String messages = getConfig().getString("Language");
-            if (!(Messages.getMessageConfig(messages.toUpperCase()) == null)) {
-                lang = messages.toUpperCase();
-                Bukkit.getConsoleSender().sendMessage("   - §fMessages: §a" + lang);
-            } else {
-                Bukkit.getConsoleSender().sendMessage("   - §fMessages: §cInvalid file! (" + messages + "), using EN");
-            }
-        }
+        // TODO: Add the secondary lang
+
+        return null;
     }
 
     ///
--- src/main/java/fr/sandro642/github/Languages/Languages.java
+++ src/main/java/fr/sandro642/github/Languages/Languages.java
@@ -1,15 +1,29 @@
 package fr.sandro642.github.Languages;
 
+import fr.sandro642.github.LangAPI.LangAPI;
+import fr.sandro642.github.Messages.Messages;
+import org.bukkit.Bukkit;
+
 public class Languages {
 
-    public static String EN;
+    public static String lang = "EN"; // Language
 
+    public void setupMessages(Boolean setup) {
+        /**
+         * Set up the LangAPI
+         */
 
-    public static void PrimaryLanguages() {
+        if (setup.equals(true)) {
 
-    }
+            Messages.load();
 
-    public static void SecondaryLanguages() {
-
+            String messages = LangAPI.getInstance().getConfig().getString("Language");
+            if (!(Messages.getMessageConfig(messages.toUpperCase()) == null)) {
+                lang = messages.toUpperCase();
+                Bukkit.getConsoleSender().sendMessage("   - §fMessages: §a" + lang);
+            } else {
+                Bukkit.getConsoleSender().sendMessage("   - §fMessages: §cInvalid file! (" + messages + "), using EN");
+            }
+        }
     }
 }
