5334a8b6c3554bea313b95e2f4479269d184872b
Sandro642 <sandro33810@gmail.com>
2023-05-25 22:39:42 +0200

    Fixed LangAPI.java, add lang.java

--- src/main/java/fr/sandro642/github/LangAPI/LangAPI.java
+++ src/main/java/fr/sandro642/github/LangAPI/LangAPI.java
@@ -12,30 +12,10 @@ public class LangAPI extends JavaPlugin {
 
     private static LangAPI setuplang;
 
-    private static String langSet;
+    public static String langSet;
 
     public static LangAPI getInstance() {
         return setuplang;
-    }
-
-    public static void setLangPlugin() {
-        String en = "EN";
-        String fr = "FR";
-        String de = "DE";
-        String es = "ES";
-        String it = "IT";
-        String nl = "NL";
-        String pl = "PL";
-        String ru = "RU";
-
-        en = langSet;
-        fr = langSet;
-        de = langSet;
-        es = langSet;
-        it = langSet;
-        nl = langSet;
-        pl = langSet;
-        ru = langSet;
     }
 
     // Get the string from /messages/file.yml and format it with color codes (hex for 1.16+)
---
+++ src/main/java/fr/sandro642/github/lang/lang.java
@@ -0,0 +1,43 @@
+package fr.sandro642.github.lang;
+
+import static fr.sandro642.github.LangAPI.LangAPI.langSet;
+
+public class lang {
+
+    public static String en = "en";
+    public static String fr = "fr";
+    public static String de = "de";
+    public static String es = "es";
+    public static String it = "it";
+    public static String nl = "nl";
+    public static String pl = "pl";
+    public static String ru = "ru";
+
+    public static void en () {
+       en = langSet;
+    }
+    public static void fr() {
+        fr = langSet;
+    }
+    public static void de() {
+        de = langSet;
+    }
+    public static void es() {
+        es = langSet;
+    }
+    public static void it() {
+        it = langSet;
+    }
+    public static void nl() {
+        nl = langSet;
+    }
+    public static void pl() {
+        pl = langSet;
+    }
+    public static void ru() {
+        ru = langSet;
+    }
+
+
+
+}
