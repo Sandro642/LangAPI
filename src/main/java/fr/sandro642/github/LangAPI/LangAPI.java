965bf1d77ea1e60a43b2062cb6a06147da7a2151
Sandro642 <sandro33810@gmail.com>
2023-05-25 22:09:27 +0200

    LangAPI.java Fixed

--- src/main/java/fr/sandro642/github/LangAPI/LangAPI.java
+++ src/main/java/fr/sandro642/github/LangAPI/LangAPI.java
@@ -18,8 +18,24 @@ public class LangAPI extends JavaPlugin {
         return setuplang;
     }
 
-    public static void setLangPlugin(String lang) {
-        lang = langSet;
+    public static void setLangPlugin() {
+        String en = "EN";
+        String fr = "FR";
+        String de = "DE";
+        String es = "ES";
+        String it = "IT";
+        String nl = "NL";
+        String pl = "PL";
+        String ru = "RU";
+
+        en = langSet;
+        fr = langSet;
+        de = langSet;
+        es = langSet;
+        it = langSet;
+        nl = langSet;
+        pl = langSet;
+        ru = langSet;
     }
 
     // Get the string from /messages/file.yml and format it with color codes (hex for 1.16+)
